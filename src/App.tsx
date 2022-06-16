bca153c775c8df437c61260d7683178a51122f4f
Naufal Akbar <kbrnugroho@gmail.com>
2022-06-16 14:29:04 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import QRCode from 'qrcode';
+
+function App() {
+  function generateQR() {
+    let canvas = document.getElementById('canvas');
+    let qrInput = document.getElementById('qrInput');
+
+    QRCode.toCanvas(canvas, qrInput.value, (err: any) => {
+      if (err) {
+        console.error(err);
+      }
+      console.log('scan berhasil')
+    });
+  }
+
+  return (
+    <div className='bg-gray-900 min-h-screen overflow-hidden'>
+      <div className='flex flex-col items-center justify-center max-w-4xl mx-auto p-56'>
+        <div className='bg-slate-100'>
+          <canvas width={100} height={100} className='border-4 border-sky-500' id='canvas'></canvas>
+        </div>
+
+        <div className='bg-slate-100 rounded p-2 my-2'>
+          <div className='flex w-full justify-between items-center mx-auto my-2'>
+            <label htmlFor='qrInput' className='mr-2'>Masukkan Teks :</label>
+            <input type='text' placeholder='Testing' id='qrInput' className='rounded p-1' onChange={generateQR} />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
